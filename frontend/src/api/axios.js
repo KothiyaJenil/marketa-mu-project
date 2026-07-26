c14d58b727c4e51197b71860bc3f2b78476b0f2c
KothiyaJenil <jenilkothiya.dev@gmail.com>
2026-07-26 16:41:28 +0530

    update api links 2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://marketa-mu-project.onrender.com',
+  baseURL: 'https://marketa-mu-project.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
